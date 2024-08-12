43c586a56f7a6bc298cf6343fb85b5dae775c8ba
Vijay Tiwari <rsvijaytiwari@gmail.com>
2024-08-12 22:32:00 +0530

    imported websocket_urlpatterns and configured URLRouter and wrapped inside AuthMiddleware

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -1,16 +1,25 @@
-"""
+'''
 ASGI config for django_chat project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
+'''
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from authentication.backends import AuthMiddleware
+from chat.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddleware(URLRouter(websocket_urlpatterns))
+})
